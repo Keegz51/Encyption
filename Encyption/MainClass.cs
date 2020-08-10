9a455e415f56ecaf34b21a427989dc6e6bdc0413
Keegan Naidoo <keegann51@gmail.com>
2020-08-11 00:32:10 +0200

    Added reading usernames and passwords from excel file into Dictionary.
    Added Encrypt and Decrypt foreach's
    
    Need to change DataStructure from Dictionary to something that allows duplicates or Dictionary of Type <string,string,string> where the first string is a unique Identifier/Key, the username is the second string and the password is the third string

--- Encyption/MainClass.cs
+++ Encyption/MainClass.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
 
 namespace Encryption
 {
@@ -13,27 +14,92 @@ namespace Encryption
 
             //Encryption
 
-            string password = "JokeR";
-            string encryptedPassword = "";
+            Dictionary<string, string> passwords = new Dictionary<string, string>();
 
+            Dictionary<string, string> OutPasswords = new Dictionary<string, string>();
+
+            //Read Excel File
+            Console.WriteLine("Enter file path?");
+            string path = Console.ReadLine();
+
+            Dictionary<string, string> InPasswords = ReadExcel(path);
+
+            //Request for Encryption/Decryption Key
             Console.WriteLine("Please enter key input1");
             int keyinput1 = Int32.Parse(Console.ReadLine());
 
             Console.WriteLine("Please enter key input2");
             int keyinput2 = Int32.Parse(Console.ReadLine());
 
-            for (int i=0;i<password.Length;i+=2)
+            //Encrypt
+            foreach (var p in InPasswords)
             {
-                encryptedPassword += Convert.ToChar(((int)password[i]+keyinput1));
+                string encryptedPassword = encryptPassword(p.Value, keyinput1, keyinput2);
+
+                passwords.Add(p.Key, encryptedPassword);
+            }
+
+            //Decrypt
+            foreach (var p in passwords)
+            {
+                string decryptedPassword = decrypt(p.Value, keyinput1, keyinput2);
+
+                OutPasswords.Add(p.Key, decryptedPassword);
+            }
+
+        }
+
+        public static Dictionary<string,string> ReadExcel(string path)
+        {
+            Dictionary<string, string> passwords = new Dictionary<string, string>();
+
+            //Loop through Excel file and add username and passwords to dictionary.
+
+            _Application excel = new Microsoft.Office.Interop.Excel.Application();
+            Workbook wb=excel.Workbooks.Open(path);
+            Worksheet ws=wb.Worksheets[1];
+
+            string u = "", p = "";
+            int i = 1;
+
+
+
+            while (ws.Cells[i,1].Value!=null)
+            {
+                u = ws.Cells[i, 1].Value;
+                p = ws.Cells[i, 2].Value;
+
+                i++;
+
+                passwords.Add(u, p);
+            }
+
+            return passwords;
+        }
+    public static string encryptPassword(string password,int key1,int key2)
+        {
+
+            string encryptedPassword = "";
+
+            for (int i = 0; i < password.Length; i += 2)
+            {
+                encryptedPassword += Convert.ToChar(((int)password[i] + key1));
             }
 
             for (int i = 1; i < password.Length; i += 2)
             {
-                encryptedPassword += Convert.ToChar(((int)password[i] + keyinput2));
+                encryptedPassword += Convert.ToChar(((int)password[i] + key2));
             }
 
+            return encryptedPassword;
+        }
 
-            int splitter = (int)Math.Ceiling((double)(encryptedPassword.Length)/2.0);
+        public static string decrypt(string encryptedPassword, int key1, int key2)
+        {
+
+            string decriptedPassword = "";
+
+            int splitter = (int)Math.Ceiling((double)(encryptedPassword.Length) / 2.0);
             Console.WriteLine(splitter);
 
             string pass1 = encryptedPassword.Substring(0, splitter);
@@ -41,30 +107,26 @@ namespace Encryption
 
             //LmT   qg
             Console.WriteLine(encryptedPassword);
-            Console.WriteLine(pass1 + " " +pass2);
+            Console.WriteLine(pass1 + " " + pass2);
             int i1 = 0;
             int i2 = 0;
 
             //Decription
-            string decriptedPassword = "";
-
             for (int i = 0; i < encryptedPassword.Length; i++)
             {
-                
-                if(i%2==0)
+
+                if (i % 2 == 0)
                 {
-                    decriptedPassword += Convert.ToChar(((int)pass1[i1] - keyinput1));
+                    decriptedPassword += Convert.ToChar(((int)pass1[i1] - key1));
                     i1++;
                 }
                 else
                 {
-                    decriptedPassword += Convert.ToChar(((int)pass2[i2] - keyinput2));
+                    decriptedPassword += Convert.ToChar(((int)pass2[i2] - key2));
                     i2++;
                 }
             }
-
-            Console.WriteLine(decriptedPassword);
-
+            return decriptedPassword;
         }
     }
 }
