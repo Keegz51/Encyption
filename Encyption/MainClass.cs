f83dd20b2cd7ddc0e8ca1d50008ba8aa7855e897
Keegan Naidoo <keegann51@gmail.com>
2020-08-15 16:47:28 +0200

    Seperated methods into new class which is inherited by the driver.
    Gave user the option to either encrypt or decrypt.
    
    Wrote decrypted and encrypted records to excel files.

--- Encyption/MainClass.cs
+++ Encyption/MainClass.cs
@@ -4,200 +4,90 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Office.Interop.Excel;
+using Encyption;
+using System.IO;
+
 
 namespace Encryption
 {
-    public class MainClass
+    public class MainClass : Methods
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Would you like to Encrypt a File or Decrypt a File?");
+            string action = Console.ReadLine();
 
-            //Encryption
-
-            //Read Excel File
-            Console.WriteLine("Enter file path?");
-            string path = Console.ReadLine();
-
-            //Read the excel file into dictionary
-            Dictionary<string, passData> InDict = ReadExceltoObject(path);
-
-            //Encrypted Password Dictionary
-            Dictionary<string, passData> OutDict = new Dictionary<string, passData>();
-
-            //Decrypted Password Dictionary
-            Dictionary<string, passData> DecryptedDict = new Dictionary<string, passData>();
-
-            //Request for Encryption/Decryption Key
-            Console.WriteLine("Please enter key input1");
-            int keyinput1 = Int32.Parse(Console.ReadLine());
-
-            Console.WriteLine("Please enter key input2");
-            int keyinput2 = Int32.Parse(Console.ReadLine());
-            //TODO : ASCII Wrapping
-
-            //Encrypt
-            Encrypt(InDict,OutDict,keyinput1,keyinput2);
-
-            //Decrypt
-            Decrypt(DecryptedDict,OutDict,keyinput1,keyinput2);
-
-        }
-
-        public static void Encrypt(Dictionary<string,passData> InDict, Dictionary<string, passData> OutDict, int key1, int key2)
-        {
-            foreach (var p in InDict)
+            switch (action)
             {
-                string encryptedPassword = encryptPassword(p.Value.password, key1, key2);
+                case "Encrypt":
+                    //Encrypt
+                    //Read Excel File
+                    Console.WriteLine("Enter file path?");
+                    string pathE = Console.ReadLine();
 
-                OutDict.Add(p.Key, new passData()
-                {
-                    username = p.Value.username,
-                    password = encryptedPassword
-                });
+                    //Read the excel file into dictionary
+                    Dictionary<string, passData> InDict = ReadExceltoObject(pathE);
+
+                    //Encrypted Password Dictionary
+                    Dictionary<string, passData> OutDict = new Dictionary<string, passData>();
+
+
+                    //Request for Encryption/Decryption Key
+                    Console.WriteLine("Please enter key input1");
+                    int keyinput1 = Int32.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Please enter key input2");
+                    int keyinput2 = Int32.Parse(Console.ReadLine());
+
+                    Encrypt(InDict, OutDict, keyinput1, keyinput2);
+
+                    Console.WriteLine("Please enter the output filename");
+                    string outputFileName = pathE.Substring(0, pathE.LastIndexOf(@"\") + 1) + Console.ReadLine() + ".xlsx";
+
+                    while (File.Exists(outputFileName))
+                    {
+                        Console.WriteLine("File Already exists please enter a diferrent file name");
+                        outputFileName = pathE.Substring(0, pathE.LastIndexOf(@"\") + 1) + Console.ReadLine() + ".xlsx";
+                    }
+
+                    WriteObjectToExcel(OutDict, outputFileName);
+                    break;
+
+                case "Decrypt":
+                    //Decrypt
+                    //Read Excel File
+                    Console.WriteLine("Enter file path?");
+                    string pathD = Console.ReadLine();
+
+                    Dictionary<string, passData> OutDictD = ReadExceltoObject(pathD);
+
+                    //Request for Encryption/Decryption Key
+                    Console.WriteLine("Please enter key input1");
+                    int Dkeyinput1 = Int32.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Please enter key input2");
+                    int Dkeyinput2 = Int32.Parse(Console.ReadLine());
+
+                    //Decrypted Password Dictionary
+                    Dictionary<string, passData> DecryptedDict = new Dictionary<string, passData>();
+
+                    Decrypt(DecryptedDict, OutDictD, Dkeyinput1, Dkeyinput2);
+
+                    Console.WriteLine("Please enter the output filename");
+                    string outputFileNameD = pathD.Substring(0, pathD.LastIndexOf(@"\") + 1) + Console.ReadLine() + ".xlsx";
+
+                    while (File.Exists(outputFileNameD))
+                    {
+                        Console.WriteLine("File Already exists please enter a diferrent file name");
+                        outputFileNameD = pathD.Substring(0, pathD.LastIndexOf(@"\") + 1) + Console.ReadLine() + ".xlsx";
+                    }
+
+                    WriteObjectToExcel(DecryptedDict, outputFileNameD);
+                    break;
+
             }
-
         }
-
-        public static void Decrypt(Dictionary<string, passData> DecryptedDict, Dictionary<string, passData> OutDict, int key1, int key2)
-        {
-            foreach (var p in OutDict)
-            {
-                string decryptedPassword = decrypt(p.Value.password, key1, key2);
-
-                DecryptedDict.Add(p.Key, new passData()
-                {
-                    username = p.Value.username,
-                    password = decryptedPassword
-                });
-            }
-        }
-
-        public static Dictionary<string, string> ReadExcel(string path)
-        {
-            Dictionary<string, string> passwords = new Dictionary<string, string>();
-
-            //Loop through Excel file and add username and passwords to dictionary.
-
-            _Application excel = new Microsoft.Office.Interop.Excel.Application();
-            Workbook wb = excel.Workbooks.Open(path);
-            Worksheet ws = wb.Worksheets[1];
-
-            string u = "", p = "";
-            int i = 1;
-
-
-
-            while (ws.Cells[i, 1].Value != null)
-            {
-                u = ws.Cells[i, 1].Value;
-                p = ws.Cells[i, 2].Value;
-
-                i++;
-
-                passwords.Add(u, p);
-            }
-
-            return passwords;
-        }
-
-        public static Dictionary<string, passData> ReadExceltoObject(string path)
-        {
-            Dictionary<string, passData> passwords = new Dictionary<string, passData>();
-
-            //Loop through Excel file and add username and passwords to dictionary.
-
-            _Application excel = new Microsoft.Office.Interop.Excel.Application();
-            Workbook wb = excel.Workbooks.Open(path);
-            Worksheet ws = wb.Worksheets[1];
-
-            string n="",u = "", p = "";
-            int i = 1;
-
-
-
-            while (ws.Cells[i, 1].Value != null)
-            {
-                n = ws.Cells[i, 1].Value;
-                u = ws.Cells[i, 2].Value;
-                p = ws.Cells[i, 3].Value;
-
-                i++;
-
-                passData pD = new passData();
-                
-                passwords.Add(n, new passData()
-                {
-                    username=u,
-                    password=p
-                });
-            }
-
-            return passwords;
-        }
-        public static string encryptPassword(string password, int key1, int key2)
-        {
-
-            string encryptedPassword = "";
-
-            for (int i = 0; i < password.Length; i += 2)
-            {
-                encryptedPassword += Convert.ToChar(((int)password[i] + key1));
-            }
-
-            for (int i = 1; i < password.Length; i += 2)
-            {
-                encryptedPassword += Convert.ToChar(((int)password[i] + key2));
-            }
-
-            return encryptedPassword;
-        }
-
-        public static string decrypt(string encryptedPassword, int key1, int key2)
-        {
-
-            string decriptedPassword = "";
-
-            int splitter = (int)Math.Ceiling((double)(encryptedPassword.Length) / 2.0);
-            Console.WriteLine(splitter);
-
-            string pass1 = encryptedPassword.Substring(0, splitter);
-            string pass2 = encryptedPassword.Substring(splitter);
-
-            //LmT   qg
-            Console.WriteLine(encryptedPassword);
-            Console.WriteLine(pass1 + " " + pass2);
-            int i1 = 0;
-            int i2 = 0;
-
-            //Decription
-            for (int i = 0; i < encryptedPassword.Length; i++)
-            {
-
-                if (i % 2 == 0)
-                {
-                    decriptedPassword += Convert.ToChar(((int)pass1[i1] - key1));
-                    i1++;
-                }
-                else
-                {
-                    decriptedPassword += Convert.ToChar(((int)pass2[i2] - key2));
-                    i2++;
-                }
-            }
-            return decriptedPassword;
-        }
-
-        
-
     }
-    public class passData
-    {
-        public string username { get; set; }
-
-        public string password { get; set; }
-    }
-
 }
 
 
---
+++ Encyption/Methods.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+
+namespace Encyption
+{
+    public class Methods
+    {
+
+        public static void Encrypt(Dictionary<string, passData> InDict, Dictionary<string, passData> OutDict, int key1, int key2)
+        {
+            foreach (var p in InDict)
+            {
+                string encryptedPassword = encryptPassword(p.Value.password, key1, key2);
+
+                OutDict.Add(p.Key, new passData()
+                {
+                    username = p.Value.username,
+                    password = encryptedPassword
+                });
+            }
+
+        }
+
+        public static void Decrypt(Dictionary<string, passData> DecryptedDict, Dictionary<string, passData> OutDict, int key1, int key2)
+        {
+            foreach (var p in OutDict)
+            {
+                string decryptedPassword = decrypt(p.Value.password, key1, key2);
+
+                DecryptedDict.Add(p.Key, new passData()
+                {
+                    username = p.Value.username,
+                    password = decryptedPassword
+                });
+            }
+        }
+
+        public static Dictionary<string, string> ReadExcel(string path)
+        {
+            Dictionary<string, string> passwords = new Dictionary<string, string>();
+
+            //Loop through Excel file and add username and passwords to dictionary.
+
+            _Application excel = new Microsoft.Office.Interop.Excel.Application();
+            Workbook wb = excel.Workbooks.Open(path);
+            Worksheet ws = wb.Worksheets[1];
+
+            string u = "", p = "";
+            int i = 1;
+
+
+
+            while (ws.Cells[i, 1].Value != null)
+            {
+                u = ws.Cells[i, 1].Value;
+                p = ws.Cells[i, 2].Value;
+
+                i++;
+
+                passwords.Add(u, p);
+            }
+
+            return passwords;
+        }
+
+        public static Dictionary<string, passData> ReadExceltoObject(string path)
+        {
+            Dictionary<string, passData> passwords = new Dictionary<string, passData>();
+
+            //Loop through Excel file and add username and passwords to dictionary.
+
+            _Application excel = new Microsoft.Office.Interop.Excel.Application();
+            Workbook wb = excel.Workbooks.Open(path);
+            Worksheet ws = wb.Worksheets[1];
+
+            string n = "", u = "", p = "";
+            int i = 1;
+
+
+
+            while (ws.Cells[i, 1].Value != null)
+            {
+                n = ws.Cells[i, 1].Value;
+                u = ws.Cells[i, 2].Value;
+                p = ws.Cells[i, 3].Value;
+
+                i++;
+
+                passData pD = new passData();
+
+                passwords.Add(n, new passData()
+                {
+                    username = u,
+                    password = p
+                });
+            }
+
+            wb.Close();
+            return passwords;
+        }
+
+        public static void WriteObjectToExcel(Dictionary<string, passData> DecryptedDict, string path)
+        {
+            //List<string, string,string> passwords = new 
+
+            _Application excel = new Microsoft.Office.Interop.Excel.Application();
+
+             Workbook wb= excel.Workbooks.Add(Type.Missing);
+            //Workbook wb = excel.;
+            Worksheet ws = wb.Worksheets[1];
+
+            string n = "", u = "", p = "";
+            int i = 1;
+
+            foreach (var d in DecryptedDict)
+            {
+                ws.Cells[i,1]=d.Key;
+                ws.Cells[i,2] = d.Value.username;
+                ws.Cells[i,3] = d.Value.password;
+
+                i++;
+            }
+
+            wb.SaveAs(path);
+            wb.Close();
+        }
+
+        public static string encryptPassword(string password, int key1, int key2)
+        {
+
+            string encryptedPassword = "";
+
+            for (int i = 0; i < password.Length; i += 2)
+            {
+                if (((int)password[i] + key1) > 255)
+                {
+
+                    encryptedPassword += Convert.ToChar((int)password[i] + key1 - 255);
+
+                }
+
+                else
+                {
+                    encryptedPassword += Convert.ToChar((int)password[i] + key1);
+                }
+            }
+
+            for (int i = 1; i < password.Length; i += 2)
+            {
+                if (((int)password[i] + key1) > 255)
+                {
+
+                    encryptedPassword += Convert.ToChar((int)password[i] + key2 - 255);
+
+                }
+
+                else
+                {
+
+                    encryptedPassword += Convert.ToChar((int)password[i] + key2);
+                }
+            }
+
+            return encryptedPassword;
+        }
+
+        public static string decrypt(string encryptedPassword, int key1, int key2)
+        {
+
+            string decriptedPassword = "";
+
+            int splitter = (int)Math.Ceiling((double)(encryptedPassword.Length) / 2.0);
+            Console.WriteLine(splitter);
+
+            string pass1 = encryptedPassword.Substring(0, splitter);
+            string pass2 = encryptedPassword.Substring(splitter);
+
+            //LmT   qg
+            Console.WriteLine(encryptedPassword);
+            Console.WriteLine(pass1 + " " + pass2);
+            int i1 = 0;
+            int i2 = 0;
+
+            //Decription
+            for (int i = 0; i < encryptedPassword.Length; i++)
+            {
+
+                if (i % 2 == 0)
+                {
+                    if (((int)pass1[i1] - key1) < 0)
+                    {
+                        decriptedPassword += Convert.ToChar(255 + (int)pass1[i1] - key1);
+                    }
+                    else
+                    {
+                        decriptedPassword += Convert.ToChar(((int)pass1[i1] - key1));
+                    }
+                    i1++;
+                }
+                else
+                {
+                    if ((int)pass1[i2] - key2 < 0)
+                    {
+                        decriptedPassword += Convert.ToChar(255 + (int)pass2[i2] - key2);
+                    }
+                    else
+                    {
+                        decriptedPassword += Convert.ToChar((int)pass2[i2] - key2);
+                    }
+                    i2++;
+                }
+            }
+            return decriptedPassword;
+        }
+    }
+    public class passData
+    {
+        public string username { get; set; }
+
+        public string password { get; set; }
+    }
+}
